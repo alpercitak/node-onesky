546514d0e68a9a2da7bea052f72136cbe8ed9a68
Alper Çıtak <citak.alper@gmail.com>
2025-07-02 13:57:45 +0200

    renames

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,21 @@ import type {
 import { isFile } from './utils';
 
 export class OneSky {
-  private PUBLIC_KEY: string;
-  private SECRET_KEY: string;
-  private API_URL = 'https://platform.api.onesky.io/1';
+  private readonly PublicKey: string;
+  private readonly SecretKey: string;
+  private readonly API_URL = 'https://platform.api.onesky.io/1';
 
   constructor(params: OneskyParams) {
-    this.PUBLIC_KEY = params.PUBLIC_KEY;
-    this.SECRET_KEY = params.SECRET_KEY;
+    this.PublicKey = params.PUBLIC_KEY;
+    this.SecretKey = params.SECRET_KEY;
   }
 
   private getAuth = (): { api_key: string; dev_hash: string; timestamp: string } => {
     const timestamp = Math.floor(Date.now() / 1000);
-    return { api_key: this.PUBLIC_KEY, dev_hash: md5(timestamp + this.SECRET_KEY), timestamp: String(timestamp) };
+    return { api_key: this.PublicKey, dev_hash: md5(timestamp + this.SecretKey), timestamp: String(timestamp) };
   };
 
-  private make_request = async (requestUrl: string, requestMethod: string, payload: RequestPayload = {}) => {
+  private makeRequest = async (requestUrl: string, requestMethod: string, payload: RequestPayload = {}) => {
     const auth = this.getAuth();
     const queryString = { ...auth };
     if (requestMethod == 'GET' || requestMethod == 'DELETE') {
@@ -78,7 +78,7 @@ export class OneSky {
      * @returns {Object}
      */
     list: (project_id: number, data: RequestPayloadFilesList) =>
-      this.make_request(`projects/${project_id}/files`, 'GET', data),
+      this.makeRequest(`projects/${project_id}/files`, 'GET', data),
 
     /**
      * Upload a file
@@ -92,7 +92,7 @@ export class OneSky {
      * @returns {Object}
      */
     upload: (project_id: number, data: RequestPayloadFilesUpload) =>
-      this.make_request(`projects/${project_id}/files`, 'POST', data),
+      this.makeRequest(`projects/${project_id}/files`, 'POST', data),
 
     /**
      * Upload a file
@@ -106,14 +106,14 @@ export class OneSky {
      * @returns {Object}
      */
     delete: (project_id: number, data: RequestPayloadFilesDelete) =>
-      this.make_request(`projects/${project_id}/files`, 'DELETE', data),
+      this.makeRequest(`projects/${project_id}/files`, 'DELETE', data),
   };
   locales = {
     /**
      * List all locales
      * @returns {Array}
      */
-    list: () => this.make_request('locales', 'GET'),
+    list: () => this.makeRequest('locales', 'GET'),
   };
   orders = {
     /**
@@ -125,7 +125,7 @@ export class OneSky {
      * @param {String=} data.file_name Filter orders by file name.
      * @returns {Array}
      */
-    list: (project_id: number, data: object) => this.make_request(`projects/${project_id}/orders`, 'GET', data),
+    list: (project_id: number, data: object) => this.makeRequest(`projects/${project_id}/orders`, 'GET', data),
 
     /**
      * Retrieve details of an order
@@ -134,7 +134,7 @@ export class OneSky {
      * @returns {Object}
      */
     show: (project_id: number, order_id: number) =>
-      this.make_request(`projects/${project_id}/orders/${order_id}`, 'GET'),
+      this.makeRequest(`projects/${project_id}/orders/${order_id}`, 'GET'),
 
     /**
      * Create a new order
@@ -152,7 +152,7 @@ export class OneSky {
      * @param {String=} data.note Note to translator
      * @returns {Object}
      */
-    create: (project_id: number, data: object) => this.make_request(`projects/${project_id}/orders`, 'POST', data),
+    create: (project_id: number, data: object) => this.makeRequest(`projects/${project_id}/orders`, 'POST', data),
   };
   project_groups = {
     /**
@@ -161,14 +161,14 @@ export class OneSky {
      * @param {Number=} query_string.per_page Set how many groups to retrieve for each time. (max: 100, min: 1)
      * @returns {Array}
      */
-    list: (data: RequestPayloadProjectGroupsList) => this.make_request('project-groups', 'GET', data),
+    list: (data: RequestPayloadProjectGroupsList) => this.makeRequest('project-groups', 'GET', data),
 
     /**
      *
      * @param {Number} project_group_id
      * @returns {Object}
      */
-    show: (project_group_id: number) => this.make_request(`project-groups/${project_group_id}`, 'GET'),
+    show: (project_group_id: number) => this.makeRequest(`project-groups/${project_group_id}`, 'GET'),
 
     /**
      *
@@ -177,28 +177,28 @@ export class OneSky {
      * @param {String=} data.locale Locale code of the project group base language. (default: en | example: zh-TW)
      * @returns {Object}
      */
-    create: (data: RequestPayloadProjectGroupsCreate) => this.make_request('project-groups', 'POST', data),
+    create: (data: RequestPayloadProjectGroupsCreate) => this.makeRequest('project-groups', 'POST', data),
 
     /**
      *
      * @param {Number} project_group_id
      * @returns
      */
-    delete: (project_group_id: number) => this.make_request(`project-groups/${project_group_id} `, 'DELETE'),
+    delete: (project_group_id: number) => this.makeRequest(`project-groups/${project_group_id} `, 'DELETE'),
 
     /**
      *
      * @param {Number} project_group_id
      * @returns
      */
-    languages: (project_group_id: number) => this.make_request(`project-groups/${project_group_id}/languages`, 'GET'),
+    languages: (project_group_id: number) => this.makeRequest(`project-groups/${project_group_id}/languages`, 'GET'),
   };
   project_types = {
     /**
      *
      * @returns {Array}
      */
-    list: () => this.make_request('project-types', 'GET'),
+    list: () => this.makeRequest('project-types', 'GET'),
   };
   projects = {
     /**
@@ -206,14 +206,14 @@ export class OneSky {
      * @param {Number} project_group_id Project Group Id
      * @returns {Array}
      */
-    list: (project_group_id: number) => this.make_request(`project-groups/${project_group_id}/projects`, 'GET'),
+    list: (project_group_id: number) => this.makeRequest(`project-groups/${project_group_id}/projects`, 'GET'),
 
     /**
      *
      * @param {Number} project_id Project Id
      * @returns {Object}
      */
-    show: (project_id: number) => this.make_request(`projects/${project_id}`, 'GET'),
+    show: (project_id: number) => this.makeRequest(`projects/${project_id}`, 'GET'),
 
     /**
      * Creates a new project under given project group
@@ -225,7 +225,7 @@ export class OneSky {
      * @returns {Object}
      */
     create: (project_group_id: number, data: RequestPayload) =>
-      this.make_request(`project-groups/${project_group_id}/projects`, 'POST', data),
+      this.makeRequest(`project-groups/${project_group_id}/projects`, 'POST', data),
 
     /**
      *
@@ -235,21 +235,21 @@ export class OneSky {
      * @param {String=} data.description Project description
      * @returns {Object}
      */
-    update: (project_id: number, data: RequestPayload) => this.make_request(`projects/${project_id}`, 'PUT', data),
+    update: (project_id: number, data: RequestPayload) => this.makeRequest(`projects/${project_id}`, 'PUT', data),
 
     /**
      *
      * @param {Number} project_id Project Id
      * @returns {Object}
      */
-    delete: (project_id: number) => this.make_request(`projects/${project_id}`, 'DELETE'),
+    delete: (project_id: number) => this.makeRequest(`projects/${project_id}`, 'DELETE'),
 
     /**
      *
      * @param {Number} project_id
      * @returns {Array}
      */
-    languages: (project_id: number) => this.make_request(`projects/${project_id}/languages`, 'GET'),
+    languages: (project_id: number) => this.makeRequest(`projects/${project_id}/languages`, 'GET'),
   };
   quotations = {
     /**
@@ -264,7 +264,7 @@ export class OneSky {
      * @param {String=} data.specialization Specify specialization in order to translate phrases in a specific area. [general | game]
      * @returns {Object}
      */
-    show: (project_id: number, data: object) => this.make_request(`projects/${project_id}/quotations`, 'GET', data),
+    show: (project_id: number, data: object) => this.makeRequest(`projects/${project_id}/quotations`, 'GET', data),
   };
   tasks = {
     /**
@@ -276,7 +276,7 @@ export class OneSky {
      * @param {String=} data.status Filter to show only import tasks of specific status with one of the followings [all*|completed|in-progress|failed]
      * @returns {Array}
      */
-    list: (project_id: number, data: object) => this.make_request(`projects/${project_id}/import-tasks`, 'GET', data),
+    list: (project_id: number, data: object) => this.makeRequest(`projects/${project_id}/import-tasks`, 'GET', data),
 
     /**
      * Show an import task
@@ -285,7 +285,7 @@ export class OneSky {
      * @returns {Object}
      */
     show: (project_id: number, import_id: number) =>
-      this.make_request(`projects/${project_id}/import-tasks/${import_id}`, 'GET'),
+      this.makeRequest(`projects/${project_id}/import-tasks/${import_id}`, 'GET'),
   };
   translations = {
     /**
@@ -298,7 +298,7 @@ export class OneSky {
      * @returns {Promise<any>}
      */
     export: (project_id: number, data: RequestPayload) =>
-      this.make_request(`projects/${project_id}/translations`, 'GET', data),
+      this.makeRequest(`projects/${project_id}/translations`, 'GET', data),
 
     /**
      * Export translations in multilingual files
@@ -310,7 +310,7 @@ export class OneSky {
      * @returns {Object}
      */
     export_multilingual: (project_id: number, data: object) =>
-      this.make_request(`projects/${project_id}/translations/multilingual`, 'GET', data),
+      this.makeRequest(`projects/${project_id}/translations/multilingual`, 'GET', data),
 
     /**
      * Export translations of App Store Description in JSON
@@ -320,7 +320,7 @@ export class OneSky {
      * @returns {Object}
      */
     export_appdescriptions: (project_id: number, data: object) =>
-      this.make_request(`projects/${project_id}/translations/app-descriptions`, 'GET', data),
+      this.makeRequest(`projects/${project_id}/translations/app-descriptions`, 'GET', data),
 
     /**
      * Translations status
@@ -332,6 +332,6 @@ export class OneSky {
      * @returns {Object}
      */
     status: (project_id: number, data: object) =>
-      this.make_request(`projects/${project_id}/translations/status`, 'GET', data),
+      this.makeRequest(`projects/${project_id}/translations/status`, 'GET', data),
   };
 }
